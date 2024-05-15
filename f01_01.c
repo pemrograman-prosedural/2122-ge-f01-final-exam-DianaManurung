01cb11f7d30872182935bf98694db11434e69397
DianaManurung <dianamanurung12345@gmail.com>
2024-05-15 13:54:52 +0700

    pair-15

--- f01_01.c
+++ f01_01.c
@@ -1,12 +1,60 @@
-// NIM - Name
-// NIM - Name
+// 12S23014 - Mikhael Valentino Gultom
+// 12S23040 - Diana Hevila Manurung
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./libs/gender.h"
 #include "./libs/dorm.h"
 #include "./libs/student.h"
 
-int main(int _argc, char **_argv)
-{
-    // codes
+int main(int argc, char **argv) {
+    char input[100];
+    int n = 0;
+    int size = 0;
+    struct dorm_t *dorm = NULL;
+    
+    int p = 0;
+    int size2 = 0;
+    struct student_t *mhs = NULL;
+
+    while (1) {
+        fgets(input, sizeof(input), stdin);
+        
+        input[strcspn(input, "\n")] = '\0';
+        
+        char *token = strtok(input, "#");
+        
+        if (strcmp(token, "---") == 0) {
+            break;
+        } else if (strcmp(token, "dorm-add") == 0) {
+            size++;
+            dorm = realloc(dorm, size * sizeof(struct dorm_t));
+            if (!dorm) {
+                fprintf(stderr, "Memory allocation failed\n");
+                exit(EXIT_FAILURE);
+            }
+            dorm[n++] = create_dorm(input);
+        } else if (strcmp(token, "dorm-print-all-detail") == 0) {
+            for (int i = 0; i < n; i++) {
+                print_dorm(dorm[i]);
+            }
+        } else if (strcmp(token, "student-add") == 0) {
+            size2++;
+            mhs = realloc(mhs, size2 * sizeof(struct student_t));
+            if (!mhs) {
+                fprintf(stderr, "Memory allocation failed\n");
+                exit(EXIT_FAILURE);
+            }
+            mhs[p++] = create_student(input);
+        } else if (strcmp(token, "student-print-all-detail") == 0) {
+            for (int i = 0; i < p; i++) {
+                print_student(mhs[i]);
+            }
+        }
+    }
+    
+    free(dorm);
+    free(mhs);
     return 0;
 }
--- libs/dorm.c
+++ libs/dorm.c
@@ -1,7 +1,91 @@
-#include "dorm.h"
+#include "dorm.h" 
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-/**
- * @brief Define the complete function definition here. Be sure to enlist the prototype of each function
- * defined here in the corresponding header file.
- *
- */
+// menggunakan function dengan struct untuk mencreate nama asramanya
+struct dorm_t create_dorm(char *input)
+{
+  // Mendeklarasikan struct dari asramanya
+  // isi dari si struct terdiri dari dormitoryname,capacity dan gender
+  struct dorm_t dorm;
+
+  // Parse the string untuk mendapatkan jenis asrama dari si student tadi
+  // setelah mendapatkan dormitory name maka akan menginput string capacity dan gendernya
+  // menggunakan strcpy untuk menyalin parse the string
+  strcpy(dorm.name, strtok(NULL, "#"));
+  dorm.capacity = atoi(strtok(NULL, "#"));
+  char *yoru = strtok(NULL, "#");
+
+  // menggunakan fungsi set gender dari jenis asramanya
+  // menggunakan fungsi dari branching dan string compare
+  if (strcmp(yoru, "male") == 0)
+  {
+    dorm.gender = GENDER_MALE;
+  }
+  else if (strcmp(yoru, "female") == 0)
+  {
+    dorm.gender = GENDER_FEMALE;
+  }
+
+  // menginisialisasi nomor resident number = 0 atau NULLL
+  dorm.residents_num = 0;
+
+  //mencreate dormitory
+  return dorm;
+}
+
+// Function to print basic information of a dormitory
+void print_dorm(struct dorm_t dorm)
+{
+  // mencetak nama si student dan capacity dari asrama tersebut
+  printf("%s|%hu|", dorm.name, dorm.capacity);
+
+  // Print the gender of the dormitory
+  if (dorm.gender == GENDER_MALE)
+  {
+    printf("male\n");
+  }
+  else if (dorm.gender == GENDER_FEMALE)
+  {
+    printf("female\n");
+  }
+}
+
+// Function to print detailed information of a dormitory
+void print_dorm_detail(struct dorm_t dorm)
+{
+  // Print the name, capacity, and gender of the dormitory
+  printf("%s|%hu|", dorm.name, dorm.capacity);
+  if (dorm.gender == GENDER_MALE)
+  {
+    printf("male|");
+  }
+  else if (dorm.gender == GENDER_FEMALE)
+  {
+    printf("female|");
+  }
+
+  // Print the number of residents in the dormitory
+  printf("%hu\n", dorm.residents_num);
+}
+
+// Function to find a dormitory by its name in an array of dormitories
+int find_dorm(char *asrama, int zdrm, struct dorm_t *dorm)
+{
+  // Initialize the index to -1 (not found)
+  int find_dorm = -1;
+
+  // Loop through the array of dormitories
+  for (int m = 0; m < zdrm; m++)
+  {
+    // If the name of the current dormitory matches the input name, update the index
+    if (strcmp(asrama, dorm[m].name)==0)
+    {
+      find_dorm = m;
+    }
+  }
+
+  // Return the index of the found dormitory, or -1 if not found
+  return find_dorm;
+}
